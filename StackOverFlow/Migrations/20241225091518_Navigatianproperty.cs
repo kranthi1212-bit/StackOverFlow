3acc633ff0b89a10a9f2c9703a5c5169cf36ad95
Kranthi <Kranthi@DESKTOP-Q5IRS1T>
2024-12-31 15:00:07 +0530

    new changes

---
+++ StackOverFlow/Migrations/20241225091518_Navigatianproperty.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackOverFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class Navigatianproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Questions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_UserId",
+                table: "Questions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_AspNetUsers_UserId",
+                table: "Questions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_AspNetUsers_UserId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_UserId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
